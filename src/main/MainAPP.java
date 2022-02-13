53f918ce7dab10d74fb1e9711229bc367e777569
AlbertNoMes <albertnotariomestres@gmail.com>
2022-02-13 18:14:39 +0100

    aleatorio par e impar

---
+++ src/main/MainAPP.java
@@ -0,0 +1,22 @@
+/* AUTOR: ALBERT NOTARIO MESTRES
+* DATA: 7/02/22
+*/
+
+package main;
+
+public class MainAPP {
+	public static void main(String[] args) {
+		try {
+			System.out.println("Generando número aleatorio");
+			int entero = (int) (Math.random() * 1000);
+			System.out.println("Numero es : " + entero);
+			if (entero % 2 == 0) {
+				throw new Exception("Se ha generado un numero par");
+			} else {
+				throw new Exception("Se ha generado un numero impar");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
