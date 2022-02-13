114942ea3b34106fbaecda952525d0b4bc0dbdfa
AlbertNoMes <albertnotariomestres@gmail.com>
2022-02-13 18:16:26 +0100

    random de 500

--- src/main/MainAPP.java
+++ src/main/MainAPP.java
@@ -8,7 +8,7 @@ public class MainAPP {
 	public static void main(String[] args) {
 		try {
 			System.out.println("Generando número aleatorio");
-			int entero = (int) (Math.random() * 1000);
+			int entero = (int) (Math.random() * 499 + 1);
 			System.out.println("Numero es : " + entero);
 			if (entero % 2 == 0) {
 				throw new Exception("Se ha generado un numero par");
